8c4d5f2496ab75927ae8e1077b008304cf91099b
wendycameronordinix <wendy.cameron@ordinix.co.uk>
2026-09-11 16:27:43 +0100

    Specify output directory in vite config
    Set output directory for build to 'dist'.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   build: {
+    outDir: 'dist',  // Make sure this is set
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
